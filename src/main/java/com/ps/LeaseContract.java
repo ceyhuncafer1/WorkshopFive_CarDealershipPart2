0f777c104bb87c2bd6d2d2def5912cd7e88328d3
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-05-19 22:33:20 -0400

    LeaseContract finished

--- src/main/java/com/ps/LeaseContract.java
+++ src/main/java/com/ps/LeaseContract.java
@@ -1,4 +1,35 @@
 package com.ps;
 
-public class LeaseContract {
+public class LeaseContract extends Contract {
+
+    private static final double leaseFeeRate = 0.07;
+    private static final double interestRate = 0.04;
+    private static final int termOfMonths = 36;
+
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+
+        super(date, customerName, customerEmail, vehicleSold);
+
+    }
+
+    @Override
+    public double getTotalPrice() {
+
+        double vehiclePrice = getVehicleSold().getPrice();
+        double leaseFee = vehiclePrice * leaseFeeRate;
+        return vehiclePrice * 0.50 + leaseFee;
+
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+
+        double vehiclePrice = getVehicleSold().getPrice();
+        double leaseFee = vehiclePrice * leaseFeeRate;
+        double loanAmount = vehiclePrice * 0.50 + leaseFee;
+        double monthlyInterestRate = interestRate / 12;
+        return loanAmount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, termOfMonths)) /
+                (Math.pow(1 + monthlyInterestRate, termOfMonths) - 1);
+
+    }
 }
